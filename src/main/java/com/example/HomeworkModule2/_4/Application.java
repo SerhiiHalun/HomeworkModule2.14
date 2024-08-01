2b4a06ee5cc0e39c1c2c31d6ab908ff4686ac568
SerhiiHalun <sergey.galun15@gmail.com>
2024-08-02 01:12:34 +0300

    Initial commit

---
+++ src/main/java/com/example/HomeworkModule2/_4/Application.java
@@ -0,0 +1,48 @@
+package com.example.HomeworkModule2._4;
+
+import com.example.HomeworkModule2._4.Entity.Note;
+import com.example.HomeworkModule2._4.Service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+	private final NoteService noteService;
+
+	@Autowired
+	public Application(NoteService noteService) {
+		this.noteService = noteService;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Note note1 = new Note();
+		note1.setTitle("First Note");
+		note1.setContent("This is the content of the first note.");
+		noteService.add(note1);
+
+		Note note2 = new Note();
+		note2.setTitle("Second Note");
+		note2.setContent("This is the content of the second note.");
+		noteService.add(note2);
+
+
+		noteService.listAll().forEach(System.out::println);
+
+
+		note1.setContent("Updated content of the first note.");
+		noteService.update(note1);
+		System.out.println(noteService.getById(note1.getId()));
+
+
+		noteService.deleteById(note2.getId());
+		System.out.println(noteService.listAll());
+	}
+}
---
+++ src/main/java/com/example/HomeworkModule2/_4/Entity/Note.java
@@ -0,0 +1,13 @@
+package com.example.HomeworkModule2._4.Entity;
+
+import lombok.Data;
+
+
+
+@Data
+public class Note {
+
+    private long id;
+    private String title;
+    private String content;
+}
